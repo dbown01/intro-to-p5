0d7f9b80b12167866dd6a843444573e021edee3b
Drew Brown <dbrown@wlapcs.org>
2017-02-28 20:28:36 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(600,400);
+    background(127,8,119);
+}
+
+function draw(){
+ line(35,80,300,200);
+ 
+ stroke(255,255,255);
+ fill(225,0,0);
+ rect(290,100,200,250);  
+ 
+ 
+ stroke(0,0,0);
+ strokeWeight(4);
+ fill(0,0,225);
+ ellipse(50,250,80,80)
+}
