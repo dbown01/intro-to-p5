52d459fef48969ef6761cd946242355dd41924cb
Drew Brown <dbrown@wlapcs.org>
2017-03-06 21:21:41 +0000

    trying to fix

--- sketch.js
+++ sketch.js
@@ -5,14 +5,35 @@ function setup(){
 
 function draw(){
  line(35,80,300,200);
+ line(200,225,200,80);
+ line(500,300,35,300);
+ line(500,80,35,200);
  
  stroke(255,255,255);
  fill(225,0,0);
- rect(290,100,200,250);  
+ rect(290,100,200,250);
  
+ stroke(51,79,124);
+ fill(0,0,0);
+ rect(300,100,20,80);
+ 
+ stroke(148,14,163);
+ fill(0,0,0);
+ rect(200,50,100,250);
  
  stroke(0,0,0);
  strokeWeight(4);
  fill(0,0,225);
- ellipse(50,250,80,80)
+ ellipse(50,250,80,80);
+ 
+ stroke (0,0,56);
+ strokeWeight(7);
+ fill(0,0,255);
+ ellipse(200,80,200,35);
+ 
+ stroke(51,79,124);
+ strokeWeight(2);
+ fill(0,0,0);
+ ellipse(80,100,24,100);
 }
+
